543b4c96b1affa0acc5b189d668f9738c7a09a40
abuzafertamboli <abutamboli7707@gmail.com>
2023-11-04 14:51:25 +0530

    Assignment 9 added

---
+++ Assignment_9/assignment-9.js
@@ -0,0 +1,10 @@
+// Task 1: Change the text color of the heading
+
+const mainHeading = document.querySelector('.caption h1');
+mainHeading.style.color = "#dc143c";
+
+
+//  Task 2: Change the color of "Add to Cart" button
+
+const button = document.querySelector('.add-to-cart');
+button.style.backgroundColor = "#dc143c";
